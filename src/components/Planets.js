cc7cdc7b8d1dd4087941dae2181efebdf8b2e658
Tosin Ayodele <devtosxn@gmail.com>
2021-09-06 13:21:37 +0100

    add fetch query to swapi API for planets

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,6 +1,19 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+
+const fetchPlanets = async () => {
+  const response = await fetch('http://swapi.dev/api/planets/');
+  return response.json();
+};
+
+// console.log(fetchPlanets());
 
 const Planets = () => {
+  const { data, status } = useQuery('planets', fetchPlanets);
+  console.log(data);
+  console.log(status);
+  //   console.log('DATA---->', data);
+  //   console.log('STATUS---->', status);
   return <div>Planets Component</div>;
 };
 
