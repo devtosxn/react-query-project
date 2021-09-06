2f7221c2a7be540de46847fb7a3ab0a442d13410
Tosin Ayodele <devtosxn@gmail.com>
2021-09-06 05:52:44 +0100

    define and add People and Planets components to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import Navbar from './components/Navbar';
+import Planets from 'components/Planets';
+import People from 'components/People';
 
 function App() {
+  const [page, setPage] = useState('planets');
   return (
     <div className="App">
       <h1>Star Wars Info</h1>
       <Navbar />
-      <div className="content"></div>
+      <div className="content">{page === 'planets' ? <Planets /> : <People />}</div>
     </div>
   );
 }
---
+++ src/components/Planets.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Planets = () => {
+  return <div>Planets Component</div>;
+};
+
+export default Planets;
