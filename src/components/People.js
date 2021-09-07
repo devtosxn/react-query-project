e040742f1ff6818bca7e9e673d73852c9453a812
Tosin Ayodele <devtosxn@gmail.com>
2021-09-07 23:13:33 +0100

    add pagination to People component using query variables

--- src/components/People.js
+++ src/components/People.js
@@ -1,16 +1,35 @@
+import { useState } from 'react';
 import { useQuery } from 'react-query';
 import Person from './Person';
 
-const fetchPeople = async () => {
-  const response = await fetch('http://swapi.dev/api/people/');
+const fetchPeople = async (page) => {
+  console.log(page);
+  const response = await fetch(`http://swapi.dev/api/people/?page=${page}`);
   return response.json();
 };
 
 const People = () => {
-  const { data, status } = useQuery('people', fetchPeople);
+  const [page, setPage] = useState(1);
+  const { data, status } = useQuery(['people', page], ({ queryKey }) => fetchPeople(queryKey[1]));
   return (
     <div>
       <h2>People Component</h2>
+      <button
+        onClick={() => {
+          if (page >= 2) {
+            setPage(page - 1);
+          }
+        }}>
+        Previous Page
+      </button>
+      <button
+        onClick={() => {
+          if (page <= 5) {
+            setPage(page + 1);
+          }
+        }}>
+        Next Page
+      </button>
       {status === 'loading' && <div>Loading...</div>}
       {status === 'error' && <div>Error!</div>}
       {status === 'success' && (
